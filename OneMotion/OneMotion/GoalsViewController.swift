d6b372936ed227d52d4cb34693673992cd2eace2
gracesubianto <gracesubianto@gmail.com>
2020-05-22 14:24:36 +1200

    Grace - added goalsviewcontroller and ui

---
+++ OneMotion/OneMotion/GoalsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GoalsViewController.swift
+//  OneMotion
+//
+//  Created by Grace Subianto on 22/05/20.
+//  Copyright © 2020 Jason Vainikolo. All rights reserved.
+//
+
+import UIKit
+
+class GoalsViewController: UIViewController{
+    
+}
