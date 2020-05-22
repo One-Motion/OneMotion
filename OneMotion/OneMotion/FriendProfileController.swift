eb995b17d7add50835fc67ac7f1d79e17926aac1
ehsaasgrover <61815658+ehsaasgrover@users.noreply.github.com>
2020-05-22 16:54:49 +1200

    added friendList and friendProfile controllers and classes

---
+++ OneMotion/OneMotion/FriendProfileController.swift
@@ -0,0 +1,16 @@
+//
+//  FriendProfileController.swift
+//  OneMotion
+//
+//  Created by Ehsaas Grover on 22/05/20.
+//  Copyright © 2020 Jason Vainikolo. All rights reserved.
+//
+
+import UIKit
+class FriendProfileController: UIViewController {
+
+    @IBOutlet weak var challengeButton: UIButton!
+    @IBOutlet weak var removeButton: UIButton!
+    @IBOutlet weak var statsButton: UIButton!
+    
+}
---
+++ OneMotion/OneMotion/FriendViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FriendViewController.swift
+//  OneMotion
+//
+//  Created by Ehsaas Grover on 22/05/20.
+//  Copyright © 2020 Jason Vainikolo. All rights reserved.
+//
+
+import UIKit
+
+class FriendViewController: UIViewController {
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var friendProfile: UIButton!
+    @IBOutlet weak var friendRequests: UIButton!
+    
+    
+    
+}
