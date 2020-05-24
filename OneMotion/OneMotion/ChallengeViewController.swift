e4fd443c4df823acb4d999547c4c317007f62acc
gracesubianto <gracesubianto@gmail.com>
2020-05-24 19:55:47 +1200

    Commenting on Code

--- OneMotion/OneMotion/ChallengeViewController.swift
+++ OneMotion/OneMotion/ChallengeViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ChallengeViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
        
+    //these are the setups to all the public variables
     @IBOutlet weak var repNumber: UITextField!
     @IBOutlet weak var setNumber: UITextField!
     @IBOutlet weak var date: UITextField!
     @IBOutlet weak var timetaken: UITextField!
     @IBOutlet weak var workout: UITextField!
+    //setting up my view picker with the options that the user can choose
     
     let workouts = ["Sit-Up", "Push-Up", "Squats", "Pull-Ups"]
     let pickerView = UIPickerView()
@@ -47,13 +49,15 @@ class ChallengeViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return workouts[row]
     }
-    
+    // this function shows the different options that the user can select from,
+    //it also contains the first responder feature that hides the keyboard when the user clicks outside of the keyboard
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         workout.text = workouts[row]
         workout.resignFirstResponder()
     }
 
-
+    //this function creates the date picker and formatts it for a user friendly version
+    // it has been edited to display only what is required to the user to save their workout
     func createDatePicker(){
         
         // allignment
@@ -80,7 +84,7 @@ class ChallengeViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         datePicker.datePickerMode = .date
     }
   
-    
+    // this function sets the done pressed button to save the data the user has selected from the createdatepicker dunction
     @objc func donePressed(){
         
         //formatting
@@ -93,16 +97,16 @@ class ChallengeViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     }
     
     
-    
+    //this function hides the keyboard when the user touches outside of the popup keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         repNumber.resignFirstResponder()
         setNumber.resignFirstResponder()
         timetaken.resignFirstResponder()
     }
     
-    
+    //this button is still under construction.
     @IBAction func saveWorkout(_ sender: Any) {
-        //this button would save the workout details into the database
+        // this button will essentially send the workout challenge created to another user of the app
     }
     
 }
--- OneMotion/OneMotion/ProgressViewController.swift
+++ OneMotion/OneMotion/ProgressViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 class ProgressViewController : UIViewController, UITableViewDelegate, UITableViewDataSource, WorkoutProtocol {
-    
+    // this is the creating of the table view which stores the users workout history
     var tableData: [String] = []
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,19 +25,19 @@ class ProgressViewController : UIViewController, UITableViewDelegate, UITableVie
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tableData.count
     }
-    
+    //this allows the user to open each workout to view the details of the workout -- still under construction. 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = self.tableData[indexPath.row]
         return cell
     }
-    
+    // this function gets the user input data from the text field in the WorkoutViewController and places it into the tableview
     func getData(data: String) {
         self.performSegue(withIdentifier: "Add Segue", sender: self)
         self.tableData.append(data)
         self.tableView.reloadData()
     }
-    
+    // this function overrides the prepare code and allows the user input to be transferred from one navigation controller to another without losing the information
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Add Segue"
         {
