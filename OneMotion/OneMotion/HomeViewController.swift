c6e178d195bf44b8d99413ebc9d1ec1edf3e8e38
ehsaasgrover <61815658+ehsaasgrover@users.noreply.github.com>
2020-06-19 19:19:39 +1200

    Colour scheme

--- OneMotion/OneMotion/HomeViewController.swift
+++ OneMotion/OneMotion/HomeViewController.swift
@@ -24,6 +24,7 @@ class HomeViewController: UIViewController {
     
     var db: OpaquePointer?
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- OneMotion/OneMotion/LoginViewController.swift
+++ OneMotion/OneMotion/LoginViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 class LoginViewController : UIViewController {
-    
+   
     @IBOutlet weak var imgImage: UIImageView!
     
     @IBOutlet weak var username: UITextField!
