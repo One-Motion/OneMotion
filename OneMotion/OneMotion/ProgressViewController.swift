f9b334b3b8fec4a1a427cfed7d8caf837b176891
Jason Vainikolo <jason.vainikolo@gmail.com>
2020-06-22 20:12:21 +1200

    updated database

--- OneMotion/OneMotion/ProgressViewController.swift
+++ OneMotion/OneMotion/ProgressViewController.swift
@@ -8,17 +8,65 @@
 
 import Foundation
 import UIKit
+import SQLite3
 
 class ProgressViewController : UIViewController, UITableViewDelegate, UITableViewDataSource, WorkoutProtocol {
-    // this is the creating of the table view which stores the users workout history
+    // this is the creating of the text view which stores the users workout history
     var tableData: [String] = []
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextView!
+    var db: OpaquePointer?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //OPENS THE CONNECTION
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("OneMotion.sqlite")
+
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Error Opening database")
+            return
+        }
+        
+        var queryStatement: OpaquePointer? = nil
+        let selectQuery = "SELECT * FROM WORKOUT"
+        if sqlite3_prepare(db, selectQuery, -1, &queryStatement, nil) == SQLITE_OK{
+            
+                    if sqlite3_step(queryStatement) == SQLITE_ROW {
+                        // 3
+                        let title =  String(describing: String(cString: sqlite3_column_text(queryStatement, 0)))
+                        let date = String(describing: String(cString: sqlite3_column_text(queryStatement, 1)))
+                        let typeOfWorkout = String(describing: String(cString: sqlite3_column_text(queryStatement, 2)))
+                        
+                        let reps = sqlite3_column_int(queryStatement, 3)
+                        let sets = sqlite3_column_int(queryStatement, 4)
+                        let TimeTaken = sqlite3_column_int(queryStatement,5)
+                        
+                        
+                        
+                        //Saves all the gathered Data into a variable
+                        print("\nQuery Result:")
+                        print("\n\(title) | \(date) | \(typeOfWorkout) | \(reps) | \(sets) | \(TimeTaken)")
+                            
+                        self.textField.text = "\(title) \n\n \(date) \n\n \(typeOfWorkout) \n\n \(reps) \n\n \(sets) \n\n \(TimeTaken)"
+                        
+                        print("Successfully gathered Data!")
+                    } else {
+                        print("\nQuery returned no results.")
+                    }
+                    } else {
+                        // 6
+                      let errorMessage = String(cString: sqlite3_errmsg(db))
+                      print("\nQuery is not prepared \(errorMessage)")
+                    }
+                    // 7
+                    sqlite3_finalize(queryStatement)
+        
+        
+        
+    }
     
     override func viewDidLoad() {
         super .viewDidLoad()
-        
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
+        self.textField.isEditable = false
         
     }
     
@@ -35,7 +83,6 @@ class ProgressViewController : UIViewController, UITableViewDelegate, UITableVie
     func getData(data: String) {
         self.performSegue(withIdentifier: "Add Segue", sender: self)
         self.tableData.append(data)
-        self.tableView.reloadData()
     }
     // this function overrides the prepare code and allows the user input to be transferred from one navigation controller to another without losing the information
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
